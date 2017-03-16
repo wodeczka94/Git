cf3ebf9e5773a14c164d6d014f523d106337e275
pawel_wojewoda <wodeczka94@gmail.com>
2017-03-16 17:16:53 +0100

    blad

--- zad6.cpp
+++ zad6.cpp
@@ -1,106 +1,22 @@
+
+#include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <set>
+#include <iterator>
 
-using namespace std;
+using namespace std ;
 
-void zad61()
+int nazwisko()
 {
-	ifstream dane("dane_6_1.txt");
-	int k = 107;
-	ofstream wypis("wyniki_6_1.txt");
-	string slowo;
-	int a;											
-	for (int i = 0; i<100; ++i)						
-	{
-		dane >> slowo;								
-		for (int j = 0; j < slowo.size(); ++j)		
-		{
-			a = slowo[j];							
-			a += k;
-			while (a > 90)
-			{
-				a -= 26;
-			}
-			slowo[j] = a;
-		}
-		wypis << slowo << endl;
-	}
-	dane.close();
-	wypis.close();
-	return;
+multiset<string> s;
+ifstream in("nazwiska.txt");
+ofstream out("nazwiska_zm.txt");
+copy(istream_iterator<string>(in),istream_iterator<string>(),inserter(s,s.begin()));
+copy(s.begin(),s.end(),ostream_iterator<string>(out,"\n"));
 }
 
-void zad62()
-{
-	ifstream dane("dane_6_2.txt");
-	ofstream wypis("wyniki_6_2.txt");
-	string slowo;
-	int a, k;
-	for (int i = 0; i < 3000; ++i)
-	{
-		dane >> slowo;
-		dane >> k;
-		for (int j = 0; j < slowo.size(); ++j)
-		{
-			a = slowo[j];
-			a -= k;
-			while (a < 65)
-			{
-				a += 26;
-			}
-			slowo[j] = a;
-		}
-		wypis << slowo << endl;
-	}
-	dane.close();
-	wypis.close();
-	return;
-}
-
-void zad63()
-{
-	ifstream dane("dane_6_3.txt");
-	ofstream wypis("wyniki_6_3.txt");
-	string slowo;
-	string zmienione;
-	int a, b, k;
-	for (int i = 0; i<3000; i++)
-	{
-		k = 0;
-		dane >> slowo;
-		dane >> zmienione;
-		a = slowo[i];
-		b = zmienione[i];
-		k = b - a;
-		if (k<0)
-			k += 26;
-		for (int j = 0; j<slowo.size(); ++j)
-		{
-			a = slowo[j];
-			b = zmienione[j];
-			a += k;
-			while (a > 90)
-			{
-				a -= 26;
-			}
-			if (a != b)
-			{
-				wypis << slowo << endl;
-				break;
-			}
-		}
-	}
-	dane.close();
-	wypis.close();
-	return;
-}
-
-int main()
-{
-
-	zad61();
-	zad62();
-	zad63();	
-
-	return 0;
+int main{
+naziwsko()
 }
